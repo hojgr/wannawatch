d3baafe62b1a6f9ee50abce9091aa976750afe20
Michal Hojgr <michal.hojgr@gmail.com>
2015-12-18 22:44:11 +0100

    Improve Flash message rendering

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,16 +1,3 @@
 defmodule Wannawatch.LayoutView do
   use Wannawatch.Web, :view
-
-	def flashes(conn) do
-		out = [:success, :error, :info] |> Enum.map(fn type ->
-			if get_flash(conn, type) do
-				encoded = get_flash(conn, type)
-				"<div class='" <> to_string(type) <> " message'>" <> encoded <> "</div>"
-			else
-				""
-			end
-		end) |> Enum.join("\n")
-
-		{:safe, out}
-	end
 end
