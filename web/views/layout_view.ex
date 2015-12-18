ca99327e5c5220639acfecde212893fc616dfffb
Michal Hojgr <michal.hojgr@gmail.com>
2015-12-18 21:33:30 +0100

    HTMLEscape flash input

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,7 +4,8 @@ defmodule Wannawatch.LayoutView do
 	def flashes(conn) do
 		out = [:success, :error, :info] |> Enum.map(fn type ->
 			if get_flash(conn, type) do
-				"<div class='" <> to_string(type) <> " message'>" <> get_flash(conn, type) <> "</div>"
+				encoded = get_flash(conn, type)
+				"<div class='" <> to_string(type) <> " message'>" <> encoded <> "</div>"
 			else
 				""
 			end
